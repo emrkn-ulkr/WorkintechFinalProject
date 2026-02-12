a00a70af602389854737025d8229fb46146dc0a4
emrkn-ulkr <officialemirkan@gmail.com>
2026-02-12 03:22:03 +0300

    Delete vite-project directory

--- vite-project/src/api/workintech.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const workintechApi = axios.create({
-    baseURL: "https://workintech-fe-ecommerce.onrender.com",
-    timeout: 20000,
-});
-
-export default workintechApi;
